2cdc9c87c556505df978995618dc17b6bc49ecc6
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-22 13:27:41 -0800

    added numbers array to NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -10,5 +10,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String [] numbers = new String[10];
+        numbers[0] = "One";
+        numbers[1] = "Two";
+        numbers[2] = "Three";
+        numbers[3] = "Four";
+        numbers[4] = "Five";
+        numbers[5] = "Six";
+        numbers[6] = "Seven";
+        numbers[7] = "Eight";
+        numbers[8] = "Nine";
+        numbers[9] = "Ten";
+
     }
 }
