205e8155e362e301cdaaf3273184e60903b67cf8
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-28 17:16:10 -0800

    deleted unnecessary and extra layouts custom classes and ArrayAdapters

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -13,28 +13,27 @@ public class  FamilyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
 
-        ArrayList<Family> family= new ArrayList<Family>();
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
-        family.add(new Family("Father", "әpә"));
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo’e"));
+        words.add(new Word("ten", "na’aacha"));
 
 
-        for (int index = 0; index <family.size(); index ++){
-            FamilyAdapter adapter = new FamilyAdapter(this, family);
-            ListView listView = findViewById(R.id.listFamily);
+        for (int index = 0; index <words.size(); index ++){
+            WordAdapter adapter = new WordAdapter(this, words);
+            ListView listView = findViewById(R.id.list);
             listView.setAdapter(adapter);;
 
         }
-
     }
 
 
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -3,12 +3,35 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo’e"));
+        words.add(new Word("ten", "na’aacha"));
+
+
+        for (int index = 0; index <words.size(); index ++){
+            WordAdapter adapter = new WordAdapter(this, words);
+            ListView listView = findViewById(R.id.list);
+            listView.setAdapter(adapter);;
+
+        }
     }
 }
